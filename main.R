04b387f727771420ec1ac01d51a1ed79b9fa2d5a
Unknown <tgarutti@gmail.com>
2018-10-10 10:18:13 +0200

    Other options

--- main.R
+++ main.R
@@ -1,13 +1,19 @@
-##----------------------------------------------------------------------------------------------------------##
+##-------------------------------------------------------------------------------##
 ## Main.
-##----------------------------------------------------------------------------------------------------------##
+##-------------------------------------------------------------------------------##
 setwd("~/Documents/Repositories/financial_derivatives/")
 source("source_file.R")
 
 asset1 <- list(s0 = 220, mu = 0.015, sigma = 0.08)
 asset2 <- list(s0 = 220, mu = 0.02, sigma = 0.11)
 
-Npaths <- n_paths(1000, 20, asset1, asset2, 0.5, 1/20)
-plot_Npaths(20, 20, asset1, asset2, 0.5, 1/20)
-payoffs <- calculate_payoffs(Npaths, c(240, 230, 210, 200))
+Npaths <- n_paths(10000, 20, asset1, asset2, 0.5, 1/20)
+# plot_Npaths(20, 20, asset1, asset2, 0.5, 1/20)
+K <- c(230, 210)
+payoffs <- calculate_payoffs(Npaths, K)
 avg_payoffs <- colMeans(payoffs)
+sim_payoffs <- simulation_study(100, 20, asset1, asset2, 0.5, 1/20, K)
+discounted_payoffs <- sim_payoffs*exp(-0.1/12)
+# plot_simulation(discounted_payoffs[1:4,], 1000)
+##-------------------------------------------------------------------------------##
+##-------------------------------------------------------------------------------##
--- simulation.R
+++ simulation.R
@@ -1,6 +1,6 @@
-##----------------------------------------------------------------------------------------------------------##
+##-------------------------------------------------------------------------------##
 ## Simulation study.
-##----------------------------------------------------------------------------------------------------------##
+##-------------------------------------------------------------------------------##
 path_sim <- function(length, asset1, asset2, corr, t)
 {
   paths <- matrix(ncol = 2, nrow = length)
@@ -58,18 +58,34 @@ calculate_payoffs <- function(ST, K)
   callMax[callMax<0] <- 0
   
   # Call on min payoff
-  callMin <- rowMin(ST)-K[2]
+  callMin <- rowMin(ST)-K[1]
   callMin[callMin<0] <- 0
   
   # Put on max payoff
-  putMax <- K[3] - rowMax(ST)
+  putMax <- K[2] - rowMax(ST)
   putMax[putMax<0] <- 0
   
   # Put on min payoff
-  putMin <- K[4] - rowMin(ST)
+  putMin <- K[2] - rowMin(ST)
   putMin[putMin<0] <- 0
   
-  return(cbind(callMax, callMin, putMax, putMin))
+  # Call on asset1 payoff
+  call1 <- ST[,1] - K[1]
+  call1[call1<0] <- 0
+  
+  # Put on asset1 payoff
+  put1 <- K[2] - ST[,1]
+  put1[put1<0] <- 0
+  
+  #Call on asset2 payoff
+  call2 <- ST[,2] - K[1]
+  call2[call2<0] <- 0
+  
+  #Put on asset2 payoff
+  put2 <- K[2] - ST[,2]
+  put2[put2<0] <- 0
+  
+  return(cbind(callMax, callMin, putMax, putMin, call1, put1, call2, put2))
 }
 
 rowMax <- function(m)
@@ -83,3 +99,39 @@ rowMin <- function(m)
   rMin <- apply(m, 1, function(i){min(i)})
   return(rMin)
 }
+
+simulation_study <- function(N, length, asset1, asset2, corr, t, K)
+{
+  sim_payoffs <- matrix(nrow = 8, ncol = N)
+  for (i in 1:N) 
+  {
+    Npaths <- n_paths(10*i, 20, asset1, asset2, 0.5, 1/20)
+    payoffs <- calculate_payoffs(Npaths, K)
+    avg_payoffs <- colMeans(payoffs)
+    sim_payoffs[,i] <- avg_payoffs
+  }
+  
+  return(sim_payoffs)
+}
+
+plot_simulation <- function(sim_payoffs, N)
+{
+  par(mfrow=c(4,1)) 
+  plot(NULL, type="n", xlab="N, number of paths simulated", ylab="Price of Call on Max", xlim=c(10, 10*N), ylim=c(6, 14))
+  grid (NULL,NULL, lty = 6, col = "gray")
+  lines(x = 10*(1:N), y = sim_payoffs[1,])
+  
+  plot(NULL, type="n", xlab="N, number of paths simulated", ylab="Price of Call on Min", xlim=c(10, 10*N), ylim=c(0, 5))
+  grid (NULL,NULL, lty = 6, col = "gray")
+  lines(x = 10*(1:N), y = sim_payoffs[2,])
+  
+  plot(NULL, type="n", xlab="N, number of paths simulated", ylab="Price of Put on Max", xlim=c(10, 10*N), ylim=c(0, 2))
+  grid (NULL,NULL, lty = 6, col = "gray")
+  lines(x = 10*(1:N), y = sim_payoffs[3,])
+  
+  plot(NULL, type="n", xlab="N, number of paths simulated", ylab="Price of Put on Min", xlim=c(10, 10*N), ylim=c(2, 8))
+  grid (NULL,NULL, lty = 6, col = "gray")
+  lines(x = 10*(1:N), y = sim_payoffs[4,])
+}
+##-------------------------------------------------------------------------------##
+##-------------------------------------------------------------------------------##
